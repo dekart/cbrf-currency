2f554ef5f1eca56a9a243deeac731aadb320eb81
DEkart <dekart@Lamptop.(none)>
2008-11-30 19:57:06 +0500

    Add MemCache rate caching

--- lib/cbrf/rates.rb
+++ lib/cbrf/rates.rb
@@ -3,12 +3,22 @@ require "open-uri"
 
 module CBRF
   class Rates
+    cattr_accessor :cache
+
     attr_reader :date, :currencies
 
     def self.for(date)
       url = "http://www.cbr.ru/currency_base/D_print.aspx?date_req=#{date.strftime('%d/%m/%Y')}"
 
-      new(Hpricot(open(url)))
+      if cache.is_a?(MemCache) and cached_rates = cache.get("cbrf_currency_#{ date }")
+        return cached_rates
+      else
+        rates = new(Hpricot(open(url)))
+
+        cache.set("cbrf_currency_#{ date }", rates, 1.day) if cache.is_a?(MemCache)
+
+        return rates
+      end
     end
 
     def initialize(document)
